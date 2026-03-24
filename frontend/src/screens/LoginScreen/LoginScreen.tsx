4fda745cc44c252a9391cfb103200d4c3a6fff0f
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 04:09:10 +0530

    feat(fe)(ui): add <LoginScreen />

---
+++ frontend/src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,40 @@
+import { Navigate, useNavigate } from "react-router-dom";
+
+import Header from "@/components/Header/Header"
+
+import { useAuthStore } from "@/store/auth";
+
+import { APP_ROUTES, STATICS } from "@/constants/constants";
+
+import type { LoginScreenProps } from "./LoginScreen.props";
+
+const LoginScreen = ({ }: LoginScreenProps) => {
+    const navigate = useNavigate();
+
+    const isLoggedIn = useAuthStore(state => state.isLoggedIn);
+    const loginHandler = useAuthStore(state => state.loginHandler);
+
+    if (isLoggedIn) {
+        return <Navigate to={APP_ROUTES.DASHBOARD_SCREEN} replace />
+    }
+
+    const onLoginClickHandler = () => {
+        loginHandler()
+        navigate(APP_ROUTES.DASHBOARD_SCREEN, {
+            replace: true
+        })
+    }
+
+    return <div className="">
+        <Header />
+        <main className="text-center">
+            <div className="mt-8">
+                <p>{STATICS.LOGIN_MANDATE}</p>
+                <br />
+                <button className="bg-[#3498db] hover:bg-[#2980b9] text-white font-semibold rounded-sm py-1 px-3 cursor-pointer" onClick={onLoginClickHandler}>login</button>
+            </div>
+        </main>
+    </div>
+}
+
+export default LoginScreen
