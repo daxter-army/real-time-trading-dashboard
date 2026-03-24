0c0a6999b7ba01ecf99457a8eafd4d4fae6f4315
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 03:45:59 +0530

    feat(fe)(platform): converted route literal strings into constants

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import LoginScreen from './screens/LoginScreen/LoginScreen';
-import DashboardScreen from './screens/DashboardScreen/DashboardScreen';
+import LoginScreen from '@/screens/LoginScreen/LoginScreen';
+import DashboardScreen from '@/screens/DashboardScreen/DashboardScreen';
+
+import { APP_ROUTES } from '@/constants/constants';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
-        <Route path="/dashboard" element={<DashboardScreen />} />
-        <Route path="*" element={<LoginScreen />} />
+        <Route path={APP_ROUTES.ROOT} element={<LoginScreen />} />
+        <Route path={APP_ROUTES.DASHBOARD_SCREEN} element={<DashboardScreen />} />
+        <Route path={APP_ROUTES.WILDCARD} element={<LoginScreen />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/constants/constants.ts
@@ -0,0 +1,33 @@
+export const HTTP_BASE_PATH = `http://localhost:${import.meta.env.VITE_BACKEND_SERVER_PORT}`
+export const WS_BASE_PATH = `ws://localhost:${import.meta.env.VITE_BACKEND_SERVER_PORT}`
+
+export const API_SUB_PATHS = {
+    STATIC_TICKERS: "/tickers/get",
+    LIVE_TICKERS: "/ws/tickers/get",
+    PERIODIC_HISTORY: "/history/get"
+}
+
+export const POLLING_INTERVAL = 2000
+
+export const HISTORY_PERIOD_1D = "1D"
+export const HISTORY_PERIOD_5D = "5D"
+
+export const CHART_TYPE_LINE = "line"
+export const CHART_TYPE_CANDLE = "candlestick"
+
+export const STATICS = {
+    CLOSING_PRICE: "closing price",
+    APP_TITLE: "My trading dashboard",
+    LOGIN_MANDATE: "To view dashboard you need to login"
+}
+
+export const LOCAL_STORAGE_KEYS = {
+    USER_LOGGED_IN: "my-rtd-user-logged-in"
+}
+
+export const APP_ROUTES = {
+    ROOT: "/",
+    WILDCARD: "*",
+    LOGIN_SCREEN: "/login",
+    DASHBOARD_SCREEN: "/dashboard",
+}
