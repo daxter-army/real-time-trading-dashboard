e706215f67847115b08f8d10a571bfda62ebff0f
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 03:29:15 +0530

    feat(ui): add <Spinner /> and <Skeleton />

---
+++ frontend/src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,9 @@
+import clsx from "clsx";
+
+import type { SkeletonProps } from "./Skeleton.props";
+
+const Skeleton = ({ customClassNames = '' }: SkeletonProps) => {
+    return <div className={clsx('animate-pulse bg-gray-300 rounded', customClassNames)} />
+}
+
+export default Skeleton
---
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,5 @@
+const Spinner = () => {
+    return <div className="w-12 h-12 border-4 border-gray-300 border-t-[#3498db] rounded-full animate-spin"></div>
+}
+
+export default Spinner
