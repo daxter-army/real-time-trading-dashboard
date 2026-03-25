238d6734f5e39bf0cb4c515df2eb0cd0ee482dda
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 18:52:52 +0530

    feat(be)(controller): update get_tickers controller logic

--- backend/controller/get_tickers.go
+++ backend/controller/get_tickers.go
@@ -2,16 +2,70 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"realtime-trading-be/mapper"
+	"realtime-trading-be/responses"
 	"realtime-trading-be/service"
+	"realtime-trading-be/store"
+	"realtime-trading-be/utils"
+
+	"github.com/gorilla/websocket"
 )
 
-// GET /tickers
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
 func GetTickers(w http.ResponseWriter, r *http.Request) {
-	data := service.GetAllTickerPrices()
+	data := service.GetTickers()
 	response := mapper.ToGetTickerResponse(data)
 
 	json.NewEncoder(w).Encode(response)
+}
+
+func GetLiveTickers(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("[GetLiveTickers] upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// create a str of all available tickers
+	tickerSymbols := ""
+	for i, ticker := range store.AvailableTickers {
+		if i == len(store.AvailableTickers) {
+			tickerSymbols += ticker.Symbol + "@trade"
+		} else {
+			tickerSymbols += ticker.Symbol + "@trade/"
+		}
+	}
+
+	_, err = store.StartLiveTickerConnection()
+	if err != nil {
+		log.Println("[GetLiveTickers] error while calling StartLiveTickerConnection():", err)
+		return
+	}
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+		live := store.GetLiveTickers()
+		resp := mapper.ToGetTickerResponse(live)
+
+		message := &responses.WebsocketMessage{
+			Type: utils.TickerUpdateEventType,
+			Data: resp,
+		}
+
+		if err := conn.WriteJSON(message); err != nil {
+			log.Println(" [GetLiveTickers] ws write error in websocket message loop:", err)
+			return
+		}
+	}
 }
