5696bffe2dbf7d3dc099a86fe9f9c03dbf883fdb
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 13:36:06 +0530

    feat(fe)(store): update useAppStore

--- frontend/src/store/app.ts
+++ frontend/src/store/app.ts
@@ -2,12 +2,18 @@ import { create } from "zustand"
 
 type AppStore = {
     selectedTicker: string | null,
-    setSelectedTicker: (ticker: string) => void
+    setSelectedTicker: (ticker: string) => void,
+    isTabVisible: boolean,
+    setIsTabVisible: (isTabVisible: boolean) => void
 }
 
 export const useAppStore = create<AppStore>((set) => ({
     selectedTicker: null,
     setSelectedTicker: (ticker: string) => {
         set({ selectedTicker: ticker })
+    },
+    isTabVisible: true,
+    setIsTabVisible: (isTabVisible: boolean) => {
+        set({ isTabVisible })
     }
 }))
