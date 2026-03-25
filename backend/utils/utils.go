a30a6bbcc9ece3acd74711f6ced6c5fc40216f24
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 18:50:50 +0530

    chore(be)(utils): update utils

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -1,23 +1,8 @@
 package utils
 
-import (
-	"math"
-	"strings"
-)
-
 const (
 	binancePrefix = "BINANCE"
 
 	TickerUpdateEventType = "ticker:update"
 	AlertTriggerEventType = "alert:trigger"
 )
-
-func NormalizeTickerName(t string) string {
-	namePrefix := binancePrefix + ":"
-
-	if strings.Contains(t, namePrefix) {
-		return strings.Replace(t, namePrefix, "", 1)
-	}
-
-	return t
-}
