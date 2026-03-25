da866d668b89d0da90f02beebc82979ae0c2b79a
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 19:16:39 +0530

    chore(be)(utils): add utils

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -5,4 +5,14 @@ const (
 
 	TickerUpdateEventType = "ticker:update"
 	AlertTriggerEventType = "alert:trigger"
+
+	BinanceHistoryURL   = "https://api.binance.com/api/v3/klines?symbol=%s&interval=%s&limit=%d"
+	BinanceWebsocketURL = "wss://stream.binance.com:9443/stream?streams=%s"
+
+	BTCUSDTTickerTye   = "BTCUSDT"
+	ETHUSDTTickerType  = "ETHUSDT"
+	SOLUSDTTickerType  = "SOLUSDT"
+	DOGEUSDTTickerType = "DOGEUSDT"
+	BNBUSDTTickerType  = "BNBUSDT"
+	LTCUSDTTickerType  = "LTCUSDT"
 )
