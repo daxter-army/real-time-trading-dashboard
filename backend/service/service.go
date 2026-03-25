a7ef4f75f8bece5631d44563617cc44800069ff5
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 19:17:19 +0530

    feat(be)(service): refactor service logic

--- backend/service/service.go
+++ backend/service/service.go
@@ -1,23 +1,55 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"realtime-trading-be/domain"
+	"realtime-trading-be/mapper"
 	"realtime-trading-be/store"
+	"realtime-trading-be/utils"
 )
 
-func GetAllTickerPrices() []*domain.TickerInfo {
-	return store.GetAllPrices()
+func GetTickers() []*domain.TickerInfo {
+	return store.GetInitialTickers()
 }
 
-func CalculatePL(ticker string, current float64) (float64, float64) {
-	prev, ok := store.GetPrevPrices(ticker)
+func GetTickerHistory(symbol string, period string) ([]*domain.TickerHistory, error) {
+	var interval string
+	var limit int
 
-	if !ok || prev == 0 {
-		return 0, 0
+	switch period {
+	case "1D":
+		interval = "5m"
+		limit = 288 // 24h * 60min
+	case "5D":
+		interval = "1h"
+		limit = 120 // 5days * 24h
+	case "1M":
+		interval = "4h"
+		limit = 180 // 30days * 6 (4h candles)
+	default:
+		interval = "1m"
 	}
 
-	change := current - prev
-	percent := (change / prev) * 100
+	url := fmt.Sprintf(utils.BinanceHistoryURL, symbol, interval, limit)
 
-	return change, percent
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("[GetTickerHistory] error while:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	var data [][]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		log.Println("[GetTickerHistory] error while unmarshalling:", err)
+		return nil, err
+	}
+
+	return mapper.ToGetHistoryDomain(data), nil
 }
