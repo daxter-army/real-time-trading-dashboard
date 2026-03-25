106875221b21e4094f941221ac8febbb8c659a49
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 19:16:10 +0530

    feat(be)(mapper): add ToGetHistoryDomain for interface{} to domain mapping

--- backend/mapper/mapper.go
+++ backend/mapper/mapper.go
@@ -1,6 +1,8 @@
 package mapper
 
 import (
+	"strconv"
+
 	"realtime-trading-be/domain"
 	"realtime-trading-be/responses"
 )
@@ -20,6 +22,30 @@ func ToGetTickerResponse(rawData []*domain.TickerInfo) []*responses.TickerDTO {
 	}
 
 	return response
+}
+
+func ToGetHistoryDomain(rawData [][]interface{}) []*domain.TickerHistory {
+	candles := []*domain.TickerHistory{}
+
+	for _, item := range rawData {
+		openTime := int64(item[0].(float64))
+		open, _ := strconv.ParseFloat(item[1].(string), 64)
+		high, _ := strconv.ParseFloat(item[2].(string), 64)
+		low, _ := strconv.ParseFloat(item[3].(string), 64)
+		closeP, _ := strconv.ParseFloat(item[4].(string), 64)
+		volume, _ := strconv.ParseFloat(item[5].(string), 64)
+
+		candles = append(candles, &domain.TickerHistory{
+			OpenTime: openTime,
+			Open:     open,
+			High:     high,
+			Low:      low,
+			Close:    closeP,
+			Volume:   volume,
+		})
+	}
+
+	return candles
 }
 
 func ToGetHistoryResponse(rawData []*domain.TickerHistory) []*responses.TickerHistoryDTO {
