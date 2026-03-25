f617871cbc78ff3ed2767c37114c055a005a9ad2
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 13:39:56 +0530

    chore(fe)(ui): minor changes in <Header />

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,31 +1,21 @@
-import { useNavigate } from "react-router-dom";
 import { IoMdLogIn, IoMdLogOut } from "react-icons/io";
 
 import { useAuthStore } from "@/store/auth";
-import { APP_ROUTES, STATICS } from "@/constants/constants";
+import { STATICS } from "@/constants/constants";
 
 import type { HeaderProps } from "./Header.props";
 
 const Header = ({ }: HeaderProps) => {
-    const navigate = useNavigate();
     const isLoggedIn = useAuthStore(state => state.isLoggedIn);
     const loginHandler = useAuthStore(state => state.loginHandler);
     const logoutHandler = useAuthStore(state => state.logoutHandler);
 
     const onLogoutClickHandler = () => {
         logoutHandler()
-
-        navigate(APP_ROUTES.LOGIN_SCREEN, {
-            replace: true
-        })
     }
 
     const onLoginClickHandler = () => {
         loginHandler()
-
-        navigate(APP_ROUTES.DASHBOARD_SCREEN, {
-            replace: true
-        })
     }
 
     return <header className="flex justify-between items-center p-2 border-b border-[#bdc3c7]">
