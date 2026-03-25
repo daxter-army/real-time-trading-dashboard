fde5776a14764d22ce1ca0ed5feaa786cddad76e
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-03-25 13:35:34 +0530

    feat(fe)(platform): add visibility check while updating the data from web sockets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from '@/components/Header/Header';
 import LoginScreen from '@/screens/LoginScreen/LoginScreen';
 import DashboardScreen from '@/screens/DashboardScreen/DashboardScreen';
 
+import { useAppStore } from './store/app';
+
 import { APP_ROUTES } from '@/constants/constants';
 
 function App() {
+  const setIsTabVisible = useAppStore(state => state.setIsTabVisible)
+
+  // subscription to check whether the tab is in focus or not
+  // to pause web socket updates
+  useEffect(() => {
+    const onVisibilityChangeHandler = () => {
+      setIsTabVisible(!document.hidden)
+    }
+
+    document.addEventListener("visibilitychange", onVisibilityChangeHandler)
+
+    return () => {
+      document.removeEventListener("visibilitychange", onVisibilityChangeHandler)
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <Header />
